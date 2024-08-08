be321a792c6bd52caea699bc3e66e03fd0d43135
Estelle Troadec <estelle.t29@gmail.com>
2024-08-08 23:29:35 +0200

    fix: login wouldn't work because of a syntaxe error. Now fixed

--- app/controllers/sessionController.js
+++ app/controllers/sessionController.js
@@ -9,50 +9,36 @@ const sessionController = {
     login: async (req, res) => {
         try {
             const { email, password } = req.body;
-            // !! Votre code à partir d'ici
 
-            // On récupère user avec le role
-
-            // Est-ce que l'utilisateur existe en BDD ?
-            // Sélectionner user avec email et inclure le role, si on ne le trouve pas :
-            //      on envoie un message d'erreur dans un objet:  {error: "Utilisateur ou mot de passe incorrect"} et on render `login` en lui passant l'erreur
-            // Sinon on continue.
-
-            // on cherche le user dans la BDD via son email
+            // search user from the database, with his email
             const user = await User.findOne({
                 where: {
                   email: email,
-                }
-            }, { // on inclue le role
+                },
                   include: 'role',
                 });
                 
-              // si aucun user n'est trouvé, on envoie une erreur  
+              // if user doesn't exist, we send an error message
               if (!user) {
                 res.render('login', { error: "Utilisateur ou mot de passe incorrect"});
-              // et on en reste là en stoppant la fonction via return  
+              // and we stop the function with a return
                 return;
               }
-
-            // Le mot de passe est il correct ?
-            // On compare le mots de passe du formulaire avec celui de l'utilisateur
-            //      Si le mot de passe est incorrect : on envoie un message d'erreur dans un objet:  {error: "Utilisateur ou mot de passe incorrect"} et on render `login` en lui passant l'erreur
             
-            
-            // on compare le MDP envoyé avec le hash en BDD
+              // compare the password in the form with the one in the database
             const passwordIsValid = await bcrypt.compare(
                 password,
                 user.password
               );
             
-            // si le renseigné dans le formulaire ne correspond pas à celui dans la BDD, on envoie une erreur
+              // if the password is not valid, send an error message
               if (!passwordIsValid) {
                 res.render('login', { error: "Utilisateur ou mot de passe incorrect"});
-              // et on s'arrête là  
+              // and we stop the function with a return  
                 return;
               }; 
 
-            // ici on a choisi de ne pas mettre le mot de passe hashé dans la session et on simplifie l'objet pour garder uniquement quelques infos utiles
+            // we chose not to store the crypted password in the session
             const formattedUser = {
               id: user.id,
               name: user.name,
@@ -61,21 +47,19 @@ const sessionController = {
               },
             } ; 
 
-            // On ajoute user a la session
+            // add user to the session
             req.session.user = formattedUser;
 
-            // On enlève le mot de passe de la session.
-
-            // !! Ne pas modifier cette ligne
+            // and redirect to the home page
             res.redirect('/');
         } catch (e) {
-            console.error(e.message);
+            console.error(e);
             res.status(500).send('Server Error');
         }
     },
 
     logout: (req, res) => {
-        // !! Votre code ici
+
         req.session.destroy();
         res.redirect('/');
     },
